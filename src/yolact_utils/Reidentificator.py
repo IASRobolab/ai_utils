cab761d95c07d6d43c3a13b9c0fa3367bf7e9e85
azunino <andrea.zunino.ext@leonardocompany.com>
2021-12-14 18:01:47 +0100

    first commit with yolact and mmt reidentificator wrapper

---
+++ src/yolact_utils/Reidentificator.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+import torch
+from torch.nn import functional as F
+from yolact_utils.mmt import models
+from yolact_utils.mmt.utils.serialization import load_checkpoint, copy_state_dict
+import sys
+
+
+class FastBaseTransform(torch.nn.Module):
+    """
+    Transform that does all operations on the GPU for super speed.
+    This doesn't support a lot of config settings and should only be used for production.
+    Maintain this as necessary.
+    """
+    MEANS = (103.94, 116.78, 123.68)
+    STD = (57.38, 57.12, 58.40)
+
+    def __init__(self):
+        super().__init__()
+        self.mean = torch.Tensor(self.MEANS).float().cuda()[None, :, None, None]
+        self.std = torch.Tensor(self.STD).float().cuda()[None, :, None, None]
+
+    def forward(self, img):
+        self.mean = self.mean.to(img.device)
+        self.std = self.std.to(img.device)
+        # img assumed to be a pytorch BGR image with channel order [n, h, w, c]
+        img_size = (256, 128)
+        img = img.permute(0, 3, 1, 2).contiguous()
+        img = F.interpolate(img, img_size, mode='bilinear', align_corners=False)
+        img = (img - self.mean) / self.std
+        img = img[:, (2, 1, 0), :, :].contiguous()
+        # Return value is in channel order [n, c, h, w] and RGB
+        return img
+
+class Reidentificator:
+
+    def __init__(self, class_target, display_target=False):
+
+        self.class_target = class_target
+        self.display_target = display_target
+
+        self.transform = torch.nn.DataParallel(FastBaseTransform()).cuda()
+        print('Loading REID model...', end='')
+        self.model_REID = models.create('resnet_ibn50a', pretrained=False, num_features=0, dropout=0, num_classes=0)
+        self.model_REID.cuda()
+        self.model_REID = torch.nn.DataParallel(self.model_REID)
+        # TODO: change weights position and thing how to do it
+        checkpoint = load_checkpoint('/home/azunino/Documents/yolact/weights/resnet_ibn_REID.tar')
+        copy_state_dict(checkpoint['state_dict'], self.model_REID)
+        self.model_REID.eval()
+        print('Done.')
+
+        self.seq_n = 0
+        self.meas_init = 100
+        self.calibrated = False
+
+        self.person_avg_feat = 0  # template feature
+
+    def calibrate_person(self, rgb, inference_output):
+        try:
+            boxes = inference_output[self.class_target]['boxes']
+        except KeyError:
+            return self.calibrated
+
+        if len(boxes) > 1:
+            print('WARNING: MORE THAN ONE PERSON DETECTED DURING CALIBRATION!')
+            self.person_avg_feat = 0
+            self.seq_n = 0
+        else:
+            print("CALIBRATING ", int(self.seq_n / self.meas_init * 100), "%", end="\r")
+            img_person = self.transform(
+                torch.from_numpy(rgb[boxes[0][1]:boxes[0][3], boxes[0][0]:boxes[0][2], :]).unsqueeze(0).cuda().float())
+            self.seq_n += 1
+            self.person_avg_feat += self.model_REID(img_person.cuda()).data.cpu()
+
+            # after meas_init measures we terminate the initialization
+            if self.seq_n > self.meas_init:
+                print('\nCALIBRATION FINISHED')
+                self.calibrated = True
+                self.person_avg_feat = self.person_avg_feat / np.linalg.norm(self.person_avg_feat)
+                self.seq_n = 0
+
+        return self.calibrated
+
+    def reidentify(self, rgb, inference_output):
+        rgb = rgb.copy()
+        if not self.calibrated:
+            sys.exit("Error: Reidentificator not calibrated!")
+
+        try:
+            boxes = inference_output[self.class_target]['boxes']
+            masks = inference_output[self.class_target]['masks']
+        except KeyError:
+            print("No target detected")
+            return rgb, None
+
+        img_persons = []
+        ### COPY THE FEATURES TEMPLATE ACCORDINGLY TO NUMBER OF DETECTED PERSONS FOR FAST DISTANCE COMPUTATION
+        person_avg_feat_temp = np.tile(self.person_avg_feat, (len(boxes), 1))
+        ### CUT THE BOUNDING BOXES OF THE DETECTED PERSONS OVER THE IMAGE
+        for id in range(len(boxes)):
+            person_bb = self.transform(
+                torch.from_numpy(rgb[boxes[id][1]:boxes[id][3], boxes[id][0]:boxes[id][2], :]).unsqueeze(
+                    0).cuda().float())
+            img_persons.append(person_bb[0])
+        img_persons = [img_person.cuda().float() for img_person in img_persons]
+        img_persons = torch.stack(img_persons, 0)
+        ### PASS THE IMAGES INSIDE THE EXTERNAL NETWORK
+        feat_pers = self.model_REID(img_persons).data.cpu()
+        ### COMPUTE FEATURES DISTANCES
+        dist = np.linalg.norm(feat_pers - person_avg_feat_temp, axis=1)
+        target_idx = np.argmin(dist)
+        # print(np.sort(dist))
+        # Todo: if dist è maggiore di una threshold allora non tornare nulla
+        if self.display_target:
+            x1, y1, x2, y2 = boxes[target_idx]
+            cv2.rectangle(rgb, (x1, y1), (x2, y2), (255, 255, 255), 5)
+
+            text_str = 'TARGET'
+
+            font_face = cv2.FONT_HERSHEY_DUPLEX
+            font_scale = 0.6
+            font_thickness = 1
+
+            text_w, text_h = cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+
+            text_pt = (x1, y1 - 3)
+            text_color = [255, 255, 255]
+
+            cv2.rectangle(rgb, (x1, y1), (x1 + text_w, y1 - text_h - 4), (0, 0, 0), -1)
+            cv2.putText(rgb, text_str, text_pt, font_face, font_scale, text_color, font_thickness,
+                        cv2.LINE_AA)
+
+        return rgb, masks[target_idx]
---
+++ src/yolact_utils/YolactInference.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+
+import sys
+
+# yolact_path = '/home/yolact/'
+yolact_path = '/home/azunino/Documents/yolact/'
+sys.path.append(yolact_path)
+from data import COLORS
+from yolact import Yolact
+from utils.augmentations import FastBaseTransform
+from utils import timer
+from utils.functions import SavePath
+from layers.output_utils import postprocess, undo_image_transformation
+
+from data import cfg, set_cfg
+
+import torch
+
+import torch.backends.cudnn as cudnn
+import argparse
+import time
+
+from collections import defaultdict
+
+import cv2
+
+
+def str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def parse_args(argv=None):
+    parser = argparse.ArgumentParser(
+        description='YOLACT COCO Evaluation')
+    parser.add_argument('--top_k', default=15, type=int,
+                        help='Further restrict the number of predictions to parse')
+    parser.add_argument('--cuda', default=True, type=str2bool,
+                        help='Use cuda to evaulate model')
+    parser.add_argument('--fast_nms', default=True, type=str2bool,
+                        help='Whether to use a faster, but not entirely correct version of NMS.')
+    parser.add_argument('--cross_class_nms', default=False, type=str2bool,
+                        help='Whether compute NMS cross-class or per-class.')
+    parser.add_argument('--display_masks', default=True, type=str2bool,
+                        help='Whether or not to display masks over bounding boxes')
+    parser.add_argument('--display_bboxes', default=True, type=str2bool,
+                        help='Whether or not to display bboxes around masks')
+    parser.add_argument('--display_text', default=True, type=str2bool,
+                        help='Whether or not to display text (class [score])')
+    parser.add_argument('--display_scores', default=True, type=str2bool,
+                        help='Whether or not to display scores in addition to classes')
+    parser.add_argument('--config', default=None,
+                        help='The config object to use.')
+    parser.add_argument('--display_lincomb', default=False, type=str2bool,
+                        help='If the config uses lincomb masks, output a visualization of how those masks are created.')
+    parser.add_argument('--mask_proto_debug', default=False, dest='mask_proto_debug', action='store_true',
+                        help='Outputs stuff for scripts/compute_mask.py.')
+    parser.add_argument('--display_fps', default=False, dest='display_fps', action='store_true',
+                        help='When displaying / saving video, draw the FPS on the frame')
+    parser.set_defaults(mask_proto_debug=False, crop=True, detect=False,
+                        display_fps=False)
+
+    global args
+    args = parser.parse_args(argv)
+
+
+color_cache = defaultdict(lambda: {})
+
+
+class YolactInference:
+
+    def __init__(self, display=False, score_threshold=0.5,
+                 trained_model='yolact_plus_resnet50_54_800000.pth', argv=None):
+        parse_args(argv)
+        self.display = display  # boolean to chose if display image results or not
+        self.score_threshold = score_threshold  # threshold used to filter the detectio results
+        trained_model = yolact_path+'/weights/'+trained_model
+        model_path = SavePath.from_str(trained_model)
+        # TODO: Bad practice? Probably want to do a name lookup instead.
+        args.config = model_path.model_name + '_config'
+        print('Config not specified. Parsed %s from the file name.\n' % args.config)
+        set_cfg(args.config)
+
+        with torch.no_grad():
+
+            if args.cuda:
+                cudnn.fastest = True
+                torch.set_default_tensor_type('torch.cuda.FloatTensor')
+            else:
+                torch.set_default_tensor_type('torch.FloatTensor')
+
+            print('Loading YOLACT model...', end='')
+            self.net = Yolact()
+            self.net.load_weights(trained_model)
+            self.net.eval()
+            print(' Done.')
+
+            if args.cuda:
+                self.net = self.net.cuda()
+
+            self.net.detect.use_fast_nms = args.fast_nms
+            self.net.detect.use_cross_class_nms = args.cross_class_nms
+            cfg.mask_proto_debug = args.mask_proto_debug
+
+    def prep_display(self, dets_out, img, class_color=True, fps_str=''):
+        img_orig = img.byte().cpu().numpy()
+        img_numpy = None
+
+        mask_alpha = 0.5
+
+        img_gpu = img / 255.0
+        h, w, _ = img.shape
+
+        with timer.env('Postprocess'):
+            save = cfg.rescore_bbox
+            cfg.rescore_bbox = True
+            t = postprocess(dets_out, w, h, visualize_lincomb=args.display_lincomb,
+                            crop_masks=args.crop,
+                            score_threshold=self.score_threshold)
+            cfg.rescore_bbox = save
+
+        with timer.env('Copy'):
+
+            idx = t[1].argsort(0, descending=True)[:args.top_k]
+
+            classes, scores, boxes = [x[idx].cpu().numpy() for x in t[:3]]
+            masks = t[3][idx]
+            masks_out = masks.detach().clone().cpu().numpy()
+
+        if self.display:
+
+            num_dets_to_consider = min(args.top_k, classes.shape[0])
+            for j in range(num_dets_to_consider):
+                if scores[j] < self.score_threshold:
+                    num_dets_to_consider = j
+                    break
+
+            # Quick and dirty lambda for selecting the color for a particular index
+            # Also keeps track of a per-gpu color cache for maximum speed
+            def get_color(j, on_gpu=None):
+                global color_cache
+                color_idx = (classes[j] * 5 if class_color else j * 5) % len(COLORS)
+
+                if on_gpu is not None and color_idx in color_cache[on_gpu]:
+                    return color_cache[on_gpu][color_idx]
+                else:
+                    color = COLORS[color_idx]
+
+                    # The image might come in as RGB or BRG, depending
+                    color = (color[2], color[1], color[0])
+                    if on_gpu is not None:
+                        color = torch.Tensor(color).to(on_gpu).float() / 255.
+                        color_cache[on_gpu][color_idx] = color
+                    return color
+
+            # First, draw the masks on the GPU where we can do it really fast
+            # Beware: very fast but possibly unintelligible mask-drawing code ahead
+            # I wish I had access to OpenGL or Vulkan but alas, I guess Pytorch tensor operations will have to suffice
+            if args.display_masks and cfg.eval_mask_branch and num_dets_to_consider > 0:
+                # After this, mask is of size [num_dets, h, w, 1]
+                masks = masks[:num_dets_to_consider, :, :, None]
+
+                # Prepare the RGB images for each mask given their color (size [num_dets, h, w, 1])
+                colors = torch.cat(
+                    [get_color(j, on_gpu=img_gpu.device.index).view(1, 1, 1, 3) for j in
+                     range(num_dets_to_consider)],
+                    dim=0)
+                masks_color = masks.repeat(1, 1, 1, 3) * colors * mask_alpha
+
+                # This is 1 everywhere except for 1-mask_alpha where the mask is
+                inv_alph_masks = masks * (-mask_alpha) + 1
+
+                # I did the math for this on pen and paper. This whole block should be equivalent to:
+                #    for j in range(num_dets_to_consider):
+                #        img_gpu = img_gpu * inv_alph_masks[j] + masks_color[j]
+                masks_color_summand = masks_color[0]
+                if num_dets_to_consider > 1:
+                    inv_alph_cumul = inv_alph_masks[:(num_dets_to_consider - 1)].cumprod(dim=0)
+                    masks_color_cumul = masks_color[1:] * inv_alph_cumul
+                    masks_color_summand += masks_color_cumul.sum(dim=0)
+
+                img_gpu = img_gpu * inv_alph_masks.prod(dim=0) + masks_color_summand
+
+            if args.display_fps:
+                # Draw the box for the fps on the GPU
+                font_face = cv2.FONT_HERSHEY_DUPLEX
+                font_scale = 0.6
+                font_thickness = 1
+
+                text_w, text_h = cv2.getTextSize(fps_str, font_face, font_scale, font_thickness)[0]
+
+                img_gpu[0:text_h + 8, 0:text_w + 8] *= 0.6  # 1 - Box alpha
+
+            # Then draw the stuff that needs to be done on the cpu
+            # Note, make sure this is a uint8 tensor or opencv will not anti alias text for whatever reason
+            img_numpy = (img_gpu * 255).byte().cpu().numpy()
+
+            if args.display_fps:
+                # Draw the text on the CPU
+                text_pt = (4, text_h + 2)
+                text_color = [255, 255, 255]
+
+                cv2.putText(img_numpy, fps_str, text_pt, font_face, font_scale, text_color, font_thickness,
+                            cv2.LINE_AA)
+
+            if num_dets_to_consider == 0:
+                return img_numpy
+
+            if args.display_text or args.display_bboxes:
+                for j in reversed(range(num_dets_to_consider)):
+                    x1, y1, x2, y2 = boxes[j, :]
+                    color = get_color(j)
+                    score = scores[j]
+
+                    if args.display_bboxes:
+                        cv2.rectangle(img_numpy, (x1, y1), (x2, y2), color, 1)
+
+                    if args.display_text:
+                        _class = cfg.dataset.class_names[classes[j]]
+                        text_str = '%s: %.2f' % (_class, score) if args.display_scores else _class
+
+                        font_face = cv2.FONT_HERSHEY_DUPLEX
+                        font_scale = 0.6
+                        font_thickness = 1
+
+                        text_w, text_h = cv2.getTextSize(text_str, font_face, font_scale, font_thickness)[0]
+
+                        text_pt = (x1, y1 - 3)
+                        text_color = [255, 255, 255]
+
+                        cv2.rectangle(img_numpy, (x1, y1), (x1 + text_w, y1 - text_h - 4), color, -1)
+                        cv2.putText(img_numpy, text_str, text_pt, font_face, font_scale, text_color,
+                                    font_thickness,
+                                    cv2.LINE_AA)
+
+        if img_numpy is None:
+            return img_orig, [classes, scores, boxes, masks_out]
+        else:
+            return img_numpy, [classes, scores, boxes, masks_out]
+
+    def img_inference(self, rgb, classes=[0]):
+        frame = torch.from_numpy(rgb).cuda().float()
+        batch = FastBaseTransform()(frame.unsqueeze(0))
+        preds = self.net(batch)
+
+        img_numpy, inference = self.prep_display(preds, frame)
+
+        inference_out = {}
+
+        for idx, cls in enumerate(inference[0]):
+            # print(idx, cls)
+            if cls not in inference_out.keys():
+                inference_out[cls] = {}
+                inference_out[cls]['scores'] = []
+                inference_out[cls]['boxes'] = []
+                inference_out[cls]['masks'] = []
+            inference_out[cls]['scores'].append(inference[1][idx])
+            inference_out[cls]['boxes'].append(inference[2][idx])
+            inference_out[cls]['masks'].append(inference[3][idx])
+
+        return img_numpy, inference_out
