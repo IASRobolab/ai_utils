4d0d979abd87e01718258d9e1b4ba74db8aa715e
Federico Rollo <rollo.f96@gmail.com>
2022-02-16 09:03:20 +0100

    changed package name to ai_utils

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['yolact_utils'],
+    packages=['ai_utils'],
     package_dir={'': 'src'}
 )
 setup(**d)
