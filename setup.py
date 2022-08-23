ee49277a87ebc2a23c478a05d7abbbe9a1764280
Federico Rollo <rollo.f96@gmail.com>
2022-08-23 14:00:04 +0200

    created new setup file for ai_utils pip installation

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['ai_utils'],
-    package_dir={'': 'src'}
+package_name = 'ai_utils'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[],
+    install_requires=['setuptools', 'opencv-python', 'mediapipe', 'matplotlib', 'torch', 'sklearn'],
+    zip_safe=True,
+    maintainer='Federico Rollo',
+    maintainer_email='rollo.f96@gmail.com',
+    description='This package contains functions useful for ML algorithm use. It creates simple interface between some used AI algorithms.',
+    license='GNU GENERAL PUBLIC LICENSE v3',
+    entry_points={},
 )
-setup(**d)
