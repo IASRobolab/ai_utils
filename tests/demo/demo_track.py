3b89f1e9341d7e84002890a29cc54c90b367bbfa
Federico Rollo <rollo.f96@gmail.com>
2023-05-10 11:09:53 +0200

    updated demo tracking

--- tests/demo/demo_track.py
+++ tests/demo/demo_track.py
@@ -7,7 +7,7 @@ import sys
 
 from camera_utils.cameras.IntelRealsense import IntelRealsense
 from ai_utils.detectors.Yolov8InferTrack import Yolov8InferTrack
-
+from ai_utils.detectors.DetectorOutput import DetectorOutput
 import numpy as np
 import cv2
 import time
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     yolo_weights="/home/azunino/Downloads/yolov8l-seg.engine"
     reid_weights="/home/azunino/Documents/yolov8_tracking/weights/osnet_x0_25_msmt17.pt"
     
-    yolo = Yolov8InferTrack(display_img=True, return_img=False, model_weights=yolo_weights, reid_weights=reid_weights)
+    yolo = Yolov8InferTrack(display_img=True, return_img=True, model_weights=yolo_weights, reid_weights=reid_weights)
 
     camera = IntelRealsense(camera_resolution=IntelRealsense.Resolution.HD)
 
@@ -30,19 +30,21 @@ if __name__ == '__main__':
         prove+=1
         img = camera.get_rgb()
 
-
+        infer: DetectorOutput
         infer = yolo.img_inference(img)
         print(1/(time.time()-start_time))
         somma += (time.time()-start_time)
-        #cv2.namedWindow("YOLO TRACKING", cv2.WINDOW_NORMAL)
-        #cv2.imshow('YOLO TRACKING', im_yolo)
-        #if cv2.waitKey(1) == ord('q'):  
-        #            exit()
+
+        if infer:
+            cv2.namedWindow("YOLO TRACKING RETURNED", cv2.WINDOW_NORMAL)
+            cv2.imshow('YOLO TRACKING RETURNED', infer.image)
+            if cv2.waitKey(1) == ord('q'):  
+                break
 
     mean_time = somma/prove
     freq = 1/mean_time
 
-    print("mean time: %.4f \nfrequency: %.4f \n" % (mean_time, freq))
+    print("\nmean time: %.4f \nfrequency: %.4f \n" % (mean_time, freq))
 
 
 
